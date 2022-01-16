c3a226aadfad1d26f7bc11e2aa3a60215d99ba9e
PanosRCng <panosrcng@gmail.com>
2022-01-16 14:53:14 +0200

    auto threshold

--- attiny85_firmware/attiny85_firmware.ino
+++ attiny85_firmware/attiny85_firmware.ino
@@ -6,13 +6,11 @@
 #define CS_SEND_PIN 1
 #define CS_RECEIVE_PIN 2
 
-#define CS_SMOOTHING 10
-#define CS_THRESHOLD 2500
-#define CS_ENABLED_DELAY 255
+#define CS_THRESHOLD_CALIBRATION_DELAY 5000
+#define CS_THRESHOLD_CALIBRATION 10
+#define CS_SMOOTHING 3
 
-#define ON_DELAY_MINUTES 1
-
-#define CHANGE_STATE_DELAY 100
+#define CHANGE_STATE_DELAY 10
 
 #define OFF 0
 #define ON 1
@@ -20,26 +18,15 @@
 
 CapacitiveSensor cSensor = CapacitiveSensor(CS_SEND_PIN, CS_RECEIVE_PIN);
 
-int cs_counter;
-long cs_sum;
+long cs_threshold;
 bool cs_enabled;
 
 int state;
-
 int change_state_counter;
 
 volatile int watchdog_counter;
 
 
-
-void cs_init()
-{
-  cSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
-
-  cs_sum = 0;
-  cs_counter = 0;
-  cs_enabled = false;
-}
 
 
 void setup_watchdog(int ii)
@@ -75,25 +62,56 @@ void setup_watchdog(int ii)
 }
 
 
-void cs_read()
-{  
-  cs_enabled = false;
-  
-  cs_sum += cSensor.capacitiveSensor(30);
-  cs_counter++;
+void cs_init()
+{
+  cSensor.set_CS_AutocaL_Millis(0xFFFFFFFF);
 
-  if(cs_counter < CS_SMOOTHING)
-  {
-    return;
+  cs_threshold = 0;
+  cs_enabled = false;
+
+  cs_setup();
+}
+
+
+void cs_setup()
+{
+  delay(CS_THRESHOLD_CALIBRATION_DELAY);
+
+  for(int i=0; i<CS_THRESHOLD_CALIBRATION; i++)
+  { 
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+    cs_threshold = (cs_threshold + cs_read()) / 2;
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
+    cs_threshold = (cs_threshold + cs_read()) / 2;
   }
 
-  if((cs_sum / CS_SMOOTHING) > CS_THRESHOLD)
+  cs_threshold = (cs_threshold * 2);
+}
+
+
+void cs_check_enabled()
+{
+  cs_enabled = false;
+
+  if(cs_read() > cs_threshold)
   {
     cs_enabled = true;
   }
+}
 
-  cs_sum = 0;
-  cs_counter = 0;
+
+long cs_read()
+{ 
+  long cs_avg = 0;
+
+  for(int i=0; i<=CS_SMOOTHING; i++)
+  {
+    cs_avg = (cs_avg + cSensor.capacitiveSensor(30)) / 2; 
+  }
+  
+  return cs_avg;
 }
 
 
@@ -130,7 +148,7 @@ void on()
 void off()
 {
   digitalWrite(LED_PIN, LOW);
-  
+
   if(change_state_disabled())
   {
     return;
@@ -147,13 +165,13 @@ void off()
 
 
 void setup()                    
-{ 
+{     
   pinMode(LED_PIN, OUTPUT);
-
-  cs_init();
 
   // ~ 10s
   setup_watchdog(9);
+
+  cs_init();
 
   state = ON;
   change_state_counter = CHANGE_STATE_DELAY+1;
@@ -163,8 +181,8 @@ void setup()
 
 
 void loop()                    
-{  
-  cs_read();
+{ 
+  cs_check_enabled();
   
   switch(state)
   {
